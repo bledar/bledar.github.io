5ad57bceecdf49692b1519036eaa24a55a78ad0a
Bledar <bledi-1@live.com>
2017-06-06 11:52:57 +0200

    Korrigjim tek nr telefonit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function () {
     success:function(data) {
         var obj = JSON.parse(data);
         var info='';
-        var social='';
+        var links='';
         var work='';
         var education='';        
         var skills='';  
@@ -13,8 +13,8 @@ $(function () {
         for (var i=0; i<obj.info.length;i++ ) { 
           info +='<li class="'+obj.info[i].label+'"><span class="label">'+obj.info[i].label+'</span><span class="value">'+obj.info[i].value+'</span></li>';
         }
-        for (var i=0; i<obj.social.length;i++) {
-          social+='<li class="'+obj.social[i].label+'"><span class="label">'+obj.social[i].label+'</span><span class="value"><a href="'+obj.social[i].link+'">'+obj.social[i].user+'</a></span>'
+        for (var i=0; i<obj.links.length;i++) {
+          links+='<li class="'+obj.links[i].label+'"><span class="label">'+obj.links[i].label+'</span><span class="value"><a href="'+obj.links[i].link+'">'+obj.links[i].user+'</a></span>'
         }
         for (var i=0; i<obj.work.length;i++) {
             work+='<li><span class="date">'+obj.work[i].date+'</span><span class="title">'+obj.work[i].title+'</span><span class="company">'+obj.work[i].commpany+'</span><span class="desc">'+obj.work[i].desc+'</span></li>';
@@ -30,7 +30,7 @@ $(function () {
         $(".title h1").text(obj.info[0].value);
         $(".title h2").text(obj.title);
         $("#info-list").html(info);
-        $("#social-list").html(social);
+        $("#social-list").html(links);
         $("#work_list").html(work);
         $("#about_me h2").html(obj.category.about_label);
         $("#about_me p").html(obj.about_me);

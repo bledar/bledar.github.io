43c8797a3a877d431e1c0c0b1c6c0afeab6c4882
bledar <bledi-1@live.com>
2017-12-05 23:04:45 +0100

    new icon and conten update

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ $(function () {
         var skills='';  
 
         for (var i=0; i<obj.info.length;i++ ) { 
-          info +='<li class="'+obj.info[i].label+'"><span class="label">'+obj.info[i].label+'</span><span class="value">'+obj.info[i].value+'</span></li>';
+          info +='<li class="'+obj.info[i].label+'"><i class="'+obj.info[i].icon+'" aria-hidden="true"></i><span class="label">'+obj.info[i].label+'</span><span class="value">'+obj.info[i].value+'</span></li>';
         }
         for (var i=0; i<obj.links.length;i++) {
-          links+='<li class="'+obj.links[i].label+'"><span class="label">'+obj.links[i].label+'</span><span class="value"><a href="'+obj.links[i].link+'">'+obj.links[i].user+'</a></span>'
+          links+='<li class="'+obj.links[i].label+'"><i class="'+obj.info[i].icon+'" aria-hidden="true"></i><span class="label">'+obj.links[i].label+'</span><span class="value"><a href="'+obj.links[i].link+'">'+obj.links[i].user+'</a></span>'
         }
         for (var i=0; i<obj.work.length;i++) {
             work+='<li><span class="date">'+obj.work[i].date+'</span><span class="title">'+obj.work[i].title+'</span><span class="company">'+obj.work[i].commpany+'</span><span class="desc">'+obj.work[i].desc+'</span></li>';
@@ -23,7 +23,7 @@ $(function () {
             education+='<li><span class="date">'+obj.education[i].years+'</span><span class="title">'+obj.education[i].title+'</span><span class="company">'+obj.education[i].school+'</span><span class="desc">'+obj.education[i].desc+'</span></li>';
         }
         for (var i=0; i<obj.skills.length;i++) {
-            skills+='<div class="itemSkill">'+obj.skills[i].name+'<span class="skillBar"><span class="skillBarProgress" style="width:'+obj.skills[i].level+'%"></span></span></div>';
+            skills+='<div class="itemSkill"><span class="skillName">'+obj.skills[i].name+'</span></div>';
         }
 
         $("title").text(obj.info[0].value+ ' | Resume');
